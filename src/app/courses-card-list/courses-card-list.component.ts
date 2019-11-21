a3a30de80dd891f86e99dcd81ad66fac35afc30a
JHades <jhades.dev@gmail.com>
2019-11-21 13:22:21 +0100

    Stripe Course

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,32 +1,49 @@
 import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {CourseDialogComponent} from "../course-dialog/course-dialog.component";
+import {Course} from '../model/course';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
 
-    @Input()
-    courses: Course[];
+  @Input()
+  courses: Course[];
 
-    @Output()
-    courseEdited = new EventEmitter();
+  @Output()
+  courseEdited = new EventEmitter();
 
-    constructor(private dialog: MatDialog) {
+  isLoggedIn: boolean;
+
+  constructor(
+    private dialog: MatDialog,
+    private afAuth: AngularFireAuth) {
+  }
+
+  ngOnInit() {
+
+    this.afAuth.authState
+      .pipe(
+        map(user => !!user)
+      )
+      .subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+
+  }
+
+  purchaseCourse(course: Course, isLoggedIn: boolean) {
+
+    if (!isLoggedIn) {
+      alert("Please login first");
+      return;
     }
 
-    ngOnInit() {
-
-    }
-
-    purchaseCourse(course:Course) {
-
-
-    }
+  }
 
 }
 
