0b785fdad67092811bc46a0f6a5122800bab3ca1
Your Name <you@example.com>
2019-12-02 13:40:23 +0100

    Stripe In Practice Course

--- init-db.ts
+++ init-db.ts
@@ -5,6 +5,13 @@ import * as firebase from 'firebase';
 
 var config = {
     // TODO copy-paste here your own config, taken from the Firebase dashboard
+    apiKey: "AIzaSyBC9AIbgFfJQPKHBgGg7xULHNWjlnW-3vs",
+    authDomain: "stripe-course-recording.firebaseapp.com",
+    databaseURL: "https://stripe-course-recording.firebaseio.com",
+    projectId: "stripe-course-recording",
+    storageBucket: "stripe-course-recording.appspot.com",
+    messagingSenderId: "909700347297",
+    appId: "1:909700347297:web:0e9e7105baf123acdd87e0"
 };
 
 console.log("Uploading data to the database with the following config:\n");
