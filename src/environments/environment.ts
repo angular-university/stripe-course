16f954b29b25ddf0b6e63afd5842c518599c3a00
Your Name <you@example.com>
2019-12-02 14:33:32 +0100

    Stripe In Practice Course

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,14 @@
 export const environment = {
   production: false,
   firebase: {
-
     // TODO copy-paste here your own config, taken from the Firebase dashboard
-
+      apiKey: "AIzaSyBC9AIbgFfJQPKHBgGg7xULHNWjlnW-3vs",
+      authDomain: "stripe-course-recording.firebaseapp.com",
+      databaseURL: "https://stripe-course-recording.firebaseio.com",
+      projectId: "stripe-course-recording",
+      storageBucket: "stripe-course-recording.appspot.com",
+      messagingSenderId: "909700347297",
+      appId: "1:909700347297:web:0e9e7105baf123acdd87e0"
   },
   api: {
     baseUrl: ""
