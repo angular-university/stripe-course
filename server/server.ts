7f5d68b15c1c3942460f174b3bb135cb96f4dfce
Your Name <you@example.com>
2019-12-03 15:09:16 +0100

    Stripe In Practice Course

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,24 @@
 
+import * as express from 'express';
+import {Application} from "express";
+
+
 
 export function initServer() {
 
+    const app:Application = express();
+
+
+    app.route("/").get((req, res) => {
+
+        res.status(200).send("<h1>API is up and running!</h1>");
+
+    });
+
+    const PORT = process.env.PORT || 9000;
+
+    app.listen(PORT, () => {
+        console.log("HTTP REST API Server running at port " + PORT);
+    });
 
 }
